9218bc1851138e5e55a4e87fcfacc188ee79564f
DAMIAN-CHEN <920296082@qq.com>
2023-09-01 16:11:03 +0800

    MPU_6050

---
+++ C8T6_MPU6050/cmake-build-debug/CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/sysmem.c.obj.d
@@ -0,0 +1,20 @@
+CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/sysmem.c.obj: \
+ D:\STM32CubeMX\ project\C8T6_MPU6050\Core\Src\sysmem.c \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\errno.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\errno.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\reent.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\_ansi.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\newlib.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\_newlib_version.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\config.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\machine\ieeefp.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\features.h \
+ d:\clion-environment\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stddef.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\_types.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\machine\_types.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\machine\_default_types.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\lock.h \
+ d:\clion-environment\10\ 2021.10\lib\gcc\arm-none-eabi\10.3.1\include\stdint.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\stdint.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\_intsup.h \
+ d:\clion-environment\10\ 2021.10\arm-none-eabi\include\sys\_stdint.h
---
+++ C8T6_MPU6050/cmake-build-debug/CMakeFiles/C8T6_MPU6050.elf.dir/DependInfo.cmake
@@ -0,0 +1,66 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Startup/startup_stm32f103c8tx.s" "D:/STM32CubeMX project/C8T6_MPU6050/cmake-build-debug/CMakeFiles/C8T6_MPU6050.elf.dir/Core/Startup/startup_stm32f103c8tx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32F103xB"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Inc"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Inc"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/CMSIS/Device/ST/STM32F1xx/Include"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/CMSIS/Include"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Bsp"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/STM32CubeMX project/C8T6_MPU6050/Bsp/mpu_6050.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Bsp/mpu_6050.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Bsp/mpu_6050.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/gpio.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/gpio.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/gpio.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/i2c.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/i2c.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/i2c.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/main.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/main.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/stm32f1xx_hal_msp.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/stm32f1xx_hal_msp.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/stm32f1xx_it.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/stm32f1xx_it.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/stm32f1xx_it.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/syscalls.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/syscalls.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/sysmem.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/sysmem.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/system_stm32f1xx.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/system_stm32f1xx.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/system_stm32f1xx.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Core/Src/usart.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/usart.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Core/Src/usart.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_cortex.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_dma.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_exti.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_flash_ex.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_gpio_ex.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_i2c.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_pwr.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_rcc_ex.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_tim_ex.c.obj.d"
+  "D:/STM32CubeMX project/C8T6_MPU6050/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj" "gcc" "CMakeFiles/C8T6_MPU6050.elf.dir/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal_uart.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
